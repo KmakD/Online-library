96400cccdc4a815a866b126bb93567c920db44c9
Dawid Kmak <Dawid Kmak@DESKTOP-KETT94I>
2021-04-12 12:52:12 +0200

    working UDP

--- lab1/src/lab1/UDPClient.java
+++ lab1/src/lab1/UDPClient.java
@@ -1,6 +1,7 @@
 package lab1;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.net.DatagramPacket;
@@ -12,22 +13,30 @@ import java.net.UnknownHostException;
 public class UDPClient {
 	public static void main ( String [] args ) {
 		try {
-			if( args . length < 2) {
+			/*if( args . length < 2) {
 				System . out . println (" Usage : UDPClient <msg > <server host name >");
 				System . exit ( -1);
+		}*/
+		while(true) {			
+			int serverPort = 9876;
+			DatagramSocket aSocket = new DatagramSocket ();
+			aSocket.setSoTimeout(3000);
+			byte [] msg = new byte [1024];
+			String mesage = "";
+			Scanner scan = new Scanner(System.in);
+			mesage = scan.nextLine();
+			System.out.println(" Sent : " + mesage);
+			msg = mesage.getBytes();
+			
+			InetAddress aHost = InetAddress.getByName ("localhost");
+			DatagramPacket request = new DatagramPacket (msg , msg.length , aHost , serverPort );
+			aSocket.send ( request );
+			byte [] buffer = new byte [1024];
+			DatagramPacket reply = new DatagramPacket ( buffer , buffer.length );
+			aSocket.receive ( reply );
+			System.out.println (" Reply : " + new String ( reply.getData(), reply.getOffset(), reply.getLength()));
+			aSocket.close ();
 		}
-		DatagramSocket aSocket = new DatagramSocket ();
-		byte [] msg = args [0]. getBytes ();
-		System . out . println (" Sent : " + args [0]);
-		InetAddress aHost = InetAddress . getByName ( args [1]);
-		int serverPort = 9876;
-		DatagramPacket request = new DatagramPacket (msg , args [0]. length () , aHost , serverPort );
-		aSocket . send ( request );
-		byte [] buffer = new byte [1024];
-		DatagramPacket reply = new DatagramPacket ( buffer , buffer . length );
-		aSocket . receive ( reply );
-		System . out . println (" Reply : " + new String ( reply . getData ()));
-		aSocket . close ();
 		} catch ( SocketException ex) {
 		Logger . getLogger ( UDPClient . class . getName ()). log ( Level . SEVERE , null , ex );
 		} catch ( UnknownHostException ex) {
--- lab1/src/lab1/UDPServer.java
+++ lab1/src/lab1/UDPServer.java
@@ -12,16 +12,29 @@ public class UDPServer {
 	public static void main ( String [] args ) {
 		try {
 		// args contain message content and server hostname
-		DatagramSocket aSocket = new DatagramSocket (9879);
+		DatagramSocket aSocket = new DatagramSocket (9876);
 		byte [] buffer = new byte [1024];
 		while ( true ) {
-		DatagramPacket request = new DatagramPacket ( buffer , buffer . length );
-		aSocket . receive ( request );
-		DatagramPacket reply = new DatagramPacket ( request . getData () ,
-		request . getLength () , request . getAddress () , request . getPort ());
-		aSocket . send ( reply );
-		//aSocket . close();
+			DatagramPacket request = new DatagramPacket ( buffer , buffer . length );
+			aSocket.receive ( request );
+			byte [] message = request.getData();
+			String msg = new String(message);
+			
+			if(msg.charAt(0) == '+') {
+				//System.out.print(msg.charAt(0));
+			}
+			if(msg.charAt(0) == '-') {
+
+			}
+			if(msg.charAt(0) == '?') {
+
+			}
+			
+			DatagramPacket reply = new DatagramPacket ( request.getData () ,
+			request.getLength () , request.getAddress () , request.getPort ());
+			aSocket.send ( reply );
 		}
+		//aSocket.close();
 		} catch ( SocketException ex) {
 		Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE , null , ex );
 		} catch ( IOException ex) {
