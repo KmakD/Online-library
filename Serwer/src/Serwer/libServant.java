fad55ae1267f44773bf4935199da5511b04d4fd1
Dawid Kmak <Dawid Kmak@DESKTOP-KETT94I>
2021-05-09 22:29:44 +0200

    Server

--- Serwer/src/Serwer/libServant.java
+++ Serwer/src/Serwer/libServant.java
@@ -1,5 +1,13 @@
 package Serwer;
 
-public class libServant{
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
+import Interfejsy.ServerInterface;
+
+public class libServant extends UnicastRemoteObject implements ServerInterface{
+	
+	public libServant() throws RemoteException{
+	}
+	
 }
