4eb5e44a8367be599810d0479a848c7f18a83ad7
Dawid Kmak <Dawid Kmak@DESKTOP-KETT94I>
2021-05-28 16:15:16 +0200

    Uzupełnione interfejsy

--- Interfejsy/src/Interfejsy/Client.java
+++ Interfejsy/src/Interfejsy/Client.java
@@ -1,25 +1,19 @@
 package Interfejsy;
 
 import java.io.Serializable;
-import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Client implements Serializable {
 	public Map<String, ClientInterface> people = new HashMap<>();
 	
-	public boolean add(String nick, ClientInterface n) {
-		System.out.println("Server.register() " + nick);
-		
+	public void add(String nick, ClientInterface n) {
 		if(!people.containsKey(nick)) {
 			people.put(nick, n);
-			return true;
 		}
-		return false;
 	}
 	
 	public boolean dlt(String nick) {
-		
 		if (people.remove(nick) != null) {
 			return true;
 		}
--- Interfejsy/src/Interfejsy/ClientInterface.java
+++ Interfejsy/src/Interfejsy/ClientInterface.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ClientInterface extends Remote {
-
+	public void inform(String msg) throws RemoteException;
 }
