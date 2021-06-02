339de8760a9ff4a7e453d47a00a8fe28b4405bd4
KmakD <73443304+KmakD@users.noreply.github.com>
2021-06-02 21:24:31 +0200

    Delete Interfejsy directory

--- Interfejsy/src/Interfejsy/ClientInterface.java
+++
@@ -1,8 +0,0 @@
-package Interfejsy;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientInterface extends Remote {
-	public void inform(String msg) throws RemoteException;
-}
